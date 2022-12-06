e081aec2ae2fa2406185bc8c5d197966ad9e10a6
Deviego <deviego4@gmail.com>
2022-12-06 10:06:32 -0300

    first commit

---
+++ src/data/categories.ts
@@ -0,0 +1,6 @@
+export const categories = {
+    food: {title: 'Alimentação', color: 'blue', expense: true},
+    rent: {title: 'Aluguel', color: 'brown', expense: true},
+    salary: {title:'Salario', color:'green', expense: false}
+}
+    
---
+++ src/data/items.ts
@@ -0,0 +1,33 @@
+import { Item } from "../types/Item"
+
+
+export const items:Item[] = [
+    {
+        date: new Date(2021, 11,15),
+        category: 'food', 
+        title: 'MacDonalds',
+        value: 25.20
+
+    },
+    {
+        date: new Date(2021, 11,15),
+        category: 'food', 
+        title: 'Fronteira',
+        value: 25.20
+
+    },
+    {
+        date: new Date(2021, 11,18),
+        category: 'rent', 
+        title: 'Aluguel',
+        value: 25.20
+
+    },
+    {
+        date: new Date(2021, 10,15),
+        category: 'salary', 
+        title: 'Salário empresa',
+        value: 25.20
+
+    }
+]
